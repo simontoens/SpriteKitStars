9ce4b59981b8362bdfbd376f5b0e88647cb465fa
Simon Toens <simontoens@gmail.com>
2013-12-22 14:43:55 -0800

    Use default background color.

--- SpriteKitStars/MyScene.m
+++ SpriteKitStars/MyScene.m
@@ -7,7 +7,6 @@
 
 - (id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
-        self.backgroundColor = [UIColor blackColor];
         [StarLayerNode add:100 starsThatTake:5 secondsToCrossAndHaveAlpha:1 toParent:self];
         [StarLayerNode add:80 starsThatTake:8 secondsToCrossAndHaveAlpha:0.8 toParent:self];
         [StarLayerNode add:60 starsThatTake:10 secondsToCrossAndHaveAlpha:0.6 toParent:self];
